6603f7b30efc46494d3ebfcb6958f47d08167de4
AlinaUshakova54 <94160318+AlinaUshakova54@users.noreply.github.com>
2021-11-19 12:09:26 +0700

    Созданы классы и объекты

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,19 +9,18 @@ public class MyFirstProgram {
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody + "!");
 
-		double len = 5;
-		System.out.println("Площадь квадрата со стороной " + len +"= " + area(len));
+		Squere s = new Squere(5);
+		System.out.println("Площадь квадрата со стороной " + s.l +"= " + area(s));
 
-		double a = 6;
-		double b = 8;
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+		Rectangle r = new Rectangle(6,8);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 	}
 
-	public static double area(double l) {
-		return l * l;
+	public static double area(Squere s) {
+		return s.l * s.l;
 	}
 
-	public static double area(double a, double b) {
-		return a * b;
+	public static double area(Rectangle r) {
+		return r.a * r.b;
 	}
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Squere.java
@@ -0,0 +1,9 @@
+package ru.stqa.pft.sandbox;
+
+public class Squere {
+    public double l;
+
+    public Squere(double l) {
+        this.l = l;
+    }
+}
