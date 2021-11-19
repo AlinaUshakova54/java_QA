29002ab29bf87bd3fe15d803894eaae5d480c31d
AlinaUshakova54 <94160318+AlinaUshakova54@users.noreply.github.com>
2021-11-19 17:53:17 +0700

    Результат выполнения заданий 2-3
    Создан класс Point, метод для подсчета дистанции между двумя точками, автотест для проверки расчета

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	
+
 	public static void main(String[] args) {
 		hello("world");
 	}
@@ -14,6 +14,15 @@ public class MyFirstProgram {
 
 		Rectangle r = new Rectangle(6,8);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+		Point p1 = new Point(6,1);
+		Point p2 = new Point(2,6);
+
+		System.out.println("Координаты точки 1 (" + p1.x + ";" + p1.y + ")");
+		System.out.println("Координаты точки 2 (" + p2.x + ";" + p2.y + ")");
+
+		System.out.println("Расстояние между точками = " + p2.distance(p1));
 	}
+
 
 }
---
+++ sandbox/src/test/java/ru/stga/pft/sandbox/PointTests.java
@@ -0,0 +1,17 @@
+package ru.stga.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox.Point;
+
+public class PointTests {
+
+    @Test
+    public void testDistance() {
+        Point p1 = new Point(6,1);
+        Point p2 = new Point(2,6);
+
+        Assert.assertEquals(p2.distance(p1), 6.4031242374328485);
+
+    }
+ }
