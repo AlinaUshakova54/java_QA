c6e7d2ad9b6295825c547cceb016d41107a41eed
AlinaUshakova54 <94160318+AlinaUshakova54@users.noreply.github.com>
2021-11-22 17:46:50 +0700

    Создание общего класса для тестов

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -0,0 +1,77 @@
+package ru.stqa.pft.addressbook;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestBase {
+    protected WebDriver driver;
+
+    @Before
+    public void setUp() {
+      System.setProperty("webdriver.gecko.driver", "C:/Devel/java_QA/addressbook-web-tests/geckodriver.exe");
+      driver = new FirefoxDriver();
+      //js = (JavascriptExecutor) driver;
+      //vars = new HashMap<String, Object>();
+      driver.get("http://localhost/addressbook/");
+      login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+      driver.findElement(By.name("user")).click();
+      driver.findElement(By.name("user")).clear();
+      driver.findElement(By.name("user")).sendKeys(username);
+      driver.findElement(By.name("pass")).click();
+      driver.findElement(By.name("pass")).clear();
+      driver.findElement(By.name("pass")).sendKeys(password);
+      driver.findElement(By.cssSelector("input:nth-child(7)")).click();
+    }
+
+    @After
+    public void tearDown() {
+        logout();
+      driver.quit();
+    }
+
+    protected void logout() {
+      driver.findElement(By.linkText("Logout")).click();
+    }
+
+    protected void returnToGroupPage() {
+      driver.findElement(By.linkText("group page")).click();
+    }
+
+    protected void submitGroupCreation() {
+      driver.findElement(By.name("submit")).click();
+    }
+
+    protected void fillGroupForm(GroupData groupData) {
+      driver.findElement(By.name("group_name")).click();
+      driver.findElement(By.name("group_name")).clear();
+      driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
+      driver.findElement(By.name("group_header")).click();
+      driver.findElement(By.name("group_header")).clear();
+      driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+      driver.findElement(By.name("group_footer")).click();
+      driver.findElement(By.name("group_footer")).clear();
+      driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    protected void initGroupCreation() {
+      driver.findElement(By.name("new")).click();
+    }
+
+    protected void gotoGroupPage() {
+      driver.findElement(By.linkText("groups")).click();
+    }
+
+    protected void deleteSelectedGroups() {
+      driver.findElement(By.name("delete")).click();
+    }
+
+    protected void selectGroup() {
+      driver.findElement(By.name("selected[]")).click();
+    }
+}
